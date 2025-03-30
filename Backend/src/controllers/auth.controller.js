32d6bb51ab662b4f475dce938d59f367636d254e
shrinuuu <123ee0288@nitrkl.ac.in>
2025-03-30 15:25:39 +0530

    add commit

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 
 export const Signup = async(req,res) => {
     const {fullName,email,password}= req.body;
